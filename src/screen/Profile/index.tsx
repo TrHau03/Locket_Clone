beff6367de96c63848b1e172aa3165cbf27a949b
Minhld-21 <100395104+Minhld-21@users.noreply.github.com>
2024-04-23 01:22:58 +0700

    profile

--- src/screen/Profile/index.tsx
+++ src/screen/Profile/index.tsx
@@ -1,12 +1,62 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import useStyles from './styles'
+import { useTheme, Divider } from '@rneui/themed'
+import BaseIcons from '@/components/base/Icon'
 
 const ProfileScreen: React.FC = () => {
+    const styles = useStyles()
+    const { theme } = useTheme()
+
     return (
-        <View style={{ flex: 1, backgroundColor: 'red' }}>
-            <Text>ProfileScreen</Text>
+        <View style={styles.container}>
+            <View style={styles.bodyHeader}>
+                <View style={styles.lineShort} />
+                <Pressable style={styles.btnAvatar}>
+                    <Image style={styles.imgAvatar} source={require('../../../assets/avatar.png')} />
+                    <View style={styles.plus}>
+                        <BaseIcons name="plus" type="Entypo" color={theme.colors.white} size={28} />
+                    </View>
+                </Pressable>
+                <Text style={styles.txtName}>Minh Duc</Text>
+                <Pressable style={styles.btnUpdateI4}>
+                    <Text style={styles.txtUpdateI4}>Sửa thông tin</Text>
+                </Pressable>
+            </View>
+            <View style={styles.body}>
+                <View style={styles.title}>
+                    <BaseIcons type="AntDesign" name="plussquareo" size={24} color={theme.colors.grey1} />
+                    <Text style={styles.txtTitle}>Thiết lập Tiện ích</Text>
+                </View>
+                <View>
+                    {ulities.map((item: any) => (
+                        <TouchableOpacity style={styles.btnChild} key={item.id}>
+                            <View style={styles.btnChildLeft}>
+                                <BaseIcons name={item.nameIcon} type={item.type} color={theme.colors.black} size={20} />
+                                <Text style={styles.txtContent}>{item.text}</Text>
+                            </View>
+                            <BaseIcons name="right" type="AntDesign" color={theme.colors.black} size={20} />
+                        </TouchableOpacity>
+                    ))}
+                </View>
+            </View>
         </View>
     )
 }
 
 export default ProfileScreen
+
+const ulities = [
+    {
+        id: 1,
+        nameIcon: 'squared-plus',
+        type: 'Entypo',
+        text: 'Thêm Tiện ích',
+    },
+    {
+        id: 2,
+        nameIcon: 'questioncircle',
+        type: 'AntDesign',
+        text: 'Hướng dẫn về Tiện ích',
+    },
+]
---
+++ src/screen/Profile/styles.ts
@@ -0,0 +1,107 @@
+import { makeStyles } from '@rneui/themed'
+import Device from '@/utils/device'
+
+const useStyles = makeStyles(({ colors, spacing }) => {
+    return {
+        btnChildLeft: {
+            flexDirection: 'row',
+            gap: 10,
+        },
+        btnChild: {
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            backgroundColor: colors.grey3,
+            paddingHorizontal: '4%',
+            paddingVertical: '4%',
+            borderRadius: 10,
+        },
+        txtContent: {
+            color: colors.black,
+            fontSize: 16,
+            fontWeight: '600',
+        },
+        txtTitle: {
+            color: colors.grey1,
+            fontSize: 18,
+            fontWeight: '700',
+        },
+        title: {
+            width: '100%',
+            height: 'auto',
+            flexDirection: 'row',
+            gap: 10,
+            alignItems: 'center',
+        },
+        txtUpdateI4: {
+            fontSize: 18,
+            color: colors.black,
+            fontWeight: '700',
+            paddingVertical: '2%',
+            paddingHorizontal: '3%',
+        },
+        btnUpdateI4: {
+            backgroundColor: colors.grey3,
+            borderRadius: 40,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: '4%',
+        },
+        txtName: {
+            color: colors.black,
+            fontSize: 28,
+            fontWeight: '600',
+            marginTop: '4%',
+        },
+        plus: {
+            position: 'absolute',
+            bottom: 0,
+            right: 10,
+            backgroundColor: colors.primary,
+            borderRadius: 50,
+            borderWidth: 2,
+            borderColor: colors.grey5,
+        },
+        imgAvatar: {
+            width: '96%',
+            height: '96%',
+            borderRadius: 85,
+        },
+        btnAvatar: {
+            height: 130,
+            width: 130,
+            borderRadius: 85,
+            borderWidth: 4,
+            borderColor: colors.primary,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: '15%',
+        },
+        lineShort: {
+            width: '10%',
+            height: 7,
+            backgroundColor: colors.grey3,
+            marginTop: '3%',
+            borderRadius: 10,
+        },
+        body: {
+            paddingHorizontal: 20,
+            paddingTop: 20,
+            width: Device.getWidthDevice(),
+            height: '100%',
+            backgroundColor: colors.grey5,
+        },
+        bodyHeader: {
+            backgroundColor: colors.grey5,
+            width: Device.getWidthDevice(),
+            alignItems: 'center',
+            marginTop: '10%',
+            borderTopRightRadius: 60,
+            borderTopLeftRadius: 60,
+        },
+        container: {
+            flex: 1,
+            backgroundColor: colors.background,
+        },
+    }
+})
+export default useStyles
